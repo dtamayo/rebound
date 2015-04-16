2f1c38ce93b9fd3cf155c3f7682f886e7d042c11
dtamayo <tamayo.daniel@gmail.com>
2015-04-16 15:08:39 -0400

    latest changes to movie

--- problems/hltaumovie/problem.py
+++ problems/hltaumovie/problem.py
@@ -19,40 +19,36 @@ G = 4*math.pi**2
 rebound.set_G(G)
 starmass = 0.55
 
-tmax = 2.e4
+tmax = 5.e4
 
 sun = rebound.Particle(m=starmass)
-rebound.particle_add(sun)
+rebound.add_particle(sun)
 
 a0s = [0.,13.6,33.3,65.1,77.3,93.0]
 mass = 5.e-4
 for a in a0s[1:]:
     p = pytools.kepler_particle(m=mass,primary=sun,a=a,anom=random.uniform(0,2*math.pi),e=0.,omega=0.,inc=0.,Omega=0.)
-    rebound.particle_add(p)
+    rebound.add_particle(p)
 
-particles = rebound.particles_get()    
+particles = rebound.get_particles()
 
 N = rebound.get_N()  
 
-taue = 1.e5
-rebound.add_e_damping([taue for i in range(N)])
-
-tprev = -100.
 r_thresh = 1000.
-output_dt = 10.
+output_dt = 500.
 rebound.move_to_center_of_momentum()
+t_output = 0.
 
 while rebound.get_t()<tmax:
     _t = rebound.get_t()
     
     rebound.step()
     breakflag = check_jumps(particles, r_thresh)
-        if breakflag is True:
-            break
-    if _t - tprev > output_dt:
-        with open('m_{0:.1e}_taue_{1:.1e}.txt'.format(mass,taue), mode='a') as f:
+    if breakflag is True:
+        break
+    if _t - t_output > output_dt:
+        with open('m_{0:.1e}.txt'.format(mass), mode='a') as f:
             for i in range(1,rebound.get_N()):
-            o = particles[i].get_orbit(particles[0])
-            f.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\n".format(i,_t,o.a,o.e,o.inc,o.Omega.o.omega,o.l,o.P,o.f))
-
-    tprev = _t
+                o = particles[i].get_orbit(particles[0])
+                f.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\n".format(i,_t,o.a,o.e,o.inc,o.Omega,o.omega,o.l,o.P,o.f))
+        t_output = _t
