ad3b5c30e74488da939686f3c0a7ed2348ddafdf
dtamayo <tamayo.daniel@gmail.com>
2015-04-20 08:22:37 -0400

    made simulation that ejects a planet

--- problems/hltaumovie/problem.py
+++ problems/hltaumovie/problem.py
@@ -19,13 +19,15 @@ G = 4*math.pi**2
 rebound.set_G(G)
 starmass = 0.55
 
-tmax = 5.e4
+tmax = 3.5e5
 
 sun = rebound.Particle(m=starmass)
 rebound.add_particle(sun)
 
+random.seed(3)
+
 a0s = [0.,13.6,33.3,65.1,77.3,93.0]
-mass = 5.e-4
+mass = 2.9e-4
 for a in a0s[1:]:
     p = pytools.kepler_particle(m=mass,primary=sun,a=a,anom=random.uniform(0,2*math.pi),e=0.,omega=0.,inc=0.,Omega=0.)
     rebound.add_particle(p)
@@ -43,11 +45,11 @@ while rebound.get_t()<tmax:
     _t = rebound.get_t()
     
     rebound.step()
-    breakflag = check_jumps(particles, r_thresh)
-    if breakflag is True:
-        break
+    #breakflag = check_jumps(particles, r_thresh)
+    #if breakflag is True:
+    #    break
     if _t - t_output > output_dt:
-        with open('m_{0:.1e}.txt'.format(mass), mode='a') as f:
+        with open('/Users/dtamayo/Desktop/hltaunores.reb'.format(mass), mode='a') as f:
             for i in range(1,rebound.get_N()):
                 o = particles[i].get_orbit(particles[0])
                 f.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\n".format(i,_t,o.a,o.e,o.inc,o.Omega,o.omega,o.l,o.P,o.f))
