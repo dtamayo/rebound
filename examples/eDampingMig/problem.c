966cd94dfb683b12212127e267c16f58df521a1b
Daniel Tamayo <tamayo.daniel@gmail.com>
2015-08-05 13:48:16 -0400

    modified C version of migration test case

--- examples/eDampingMig/problem.c
+++ examples/eDampingMig/problem.c
@@ -16,40 +16,42 @@
 
 void heartbeat(struct reb_simulation* const r);
 
-double tmax = 1.e4;
+double tmax = 1.e5;
 
 int main(int argc, char* argv[]){
 	struct reb_simulation* r = reb_create_simulation();
 	// Setup constants
-	r->dt 			= 1e-2;		// initial timestep.
+	r->dt 			= 0.012;		// initial timestep.
 	r->integrator	= REB_INTEGRATOR_WHFAST;
 
 	// Setup callback function for velocity dependent forces.
-	r->post_timestep_modifications 	= reboundxf_modify_elements;
+	//r->post_timestep_modifications 	= reboundxf_modify_elements;
 	r->force_is_velocity_dependent = 1;
+	r-> G = 4*M_PI*M_PI;
 	// Setup callback function for outputs.
 	r->heartbeat	= heartbeat;
 	//r->usleep		= 1;		// Slow down integration (for visualization only)
-
-	printf("%d\n", r->ri_whfast.safe_mode);
-	double e0 = 0.1;
-	double ainner = 1.;
-	double aouter = 10.;
 
 	struct reb_particle p; 
 	p.m  	= 1.;	
 	reb_add(r, p); 
 
-	struct reb_particle p1 = reb_tools_init_orbit2d(r->G, 1., 1.e-6, ainner, e0, 0., 0.);	
-	struct reb_particle p2 = reb_tools_init_orbit2d(r->G, 1., 1.e-6, aouter, e0, 0., 0.);	
+	struct reb_particle p1 = reb_tools_init_orbit2d(r->G, 1., 1.e-8, 1.0, 0.0, 0., 0.);	
+	struct reb_particle p2 = reb_tools_init_orbit2d(r->G, 1., 1.e-5, pow(2.1,(2./3.)), 0., 0., 0.);	
 	reb_add(r,p1);
 	reb_add(r,p2);
 
-	double tau_a[3] = {0., 1.e3, 1.e3};
-	set_migration(tau_a, 3);
+	double tau_e[3] = {0.,1.e5,100.};
+	double tau_a[3] = {0., 1.e10, 1.e7};
+	double tau_po[3] = {0.,1.e5,100.};
 
+	/*reboundxf_set_migration(tau_a, 3);
+	reboundxf_set_e_damping_p(1.);
+	reboundxf_set_e_damping(tau_e,3);
+	reboundxf_set_peri_precession(tau_po,3);
+	*/
 	reb_move_to_com(r);
-	
+
 	// Delete previous output
 	system("rm -v r.txt");	
 
