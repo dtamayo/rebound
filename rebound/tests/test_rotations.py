2f29b012ba185ce233e390933ac1c34496ce2c4f
Daniel Tamayo <dtamayo@hmc.edu>
2023-01-03 11:56:25 -0800

    fixed new_axes and updated to_orbital unit test

--- rebound/tests/test_rotations.py
+++ rebound/tests/test_rotations.py
@@ -30,14 +30,14 @@ class TestRotations(unittest.TestCase):
     def test_to_orbital(self):
         sim = rebound.Simulation()
         a, e, inc, Omega, omega = 1, 0.1, 0.2, 0.3, 0.4
-        sim.add(m=1)
-        sim.add(a=a,e=e,inc=inc,Omega=Omega,omega=omega,f=0)
-        # when we rotate our particle's xyz to_orbital (x=toward peri, z=orb normal) we should get (a(1-e), 0, 0)
         r = rebound.Rotation.to_orbital(Omega=Omega, inc=inc, omega=omega)
-        res = r*sim.particles[1].xyz
-        self.assertAlmostEqual(res[0], a*(1-e), delta=1e-15)
-        self.assertAlmostEqual(res[1], 0, delta=1e-15)
-        self.assertAlmostEqual(res[2], 0, delta=1e-15)
+        res = r*[1,0,0] # When we rotate the [1,0,0] vector (x in our orbital system), we should get back a vector pointing toward peri
+        sim.add(m=1)
+        sim.add(a=a,e=e,inc=inc,Omega=Omega,omega=omega,f=0) # Initialize a particle at pericenter with same orbital angles
+        ps = sim.particles
+        self.assertAlmostEqual(res[0], ps[1].x/ps[1].d, delta=1e-15)
+        self.assertAlmostEqual(res[1], ps[1].y/ps[1].d, delta=1e-15)
+        self.assertAlmostEqual(res[2], ps[1].z/ps[1].d, delta=1e-15)
     
     def test_to_new_axes(self):
         sim = rebound.Simulation()
